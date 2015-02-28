a07e1e48a4d1f821c06b9a07d482a042c5cbfe92
baixiaodong <baihsiang2010.d@gmail.com>
2015-02-28 15:34:17 +0800

    app-framework 代码

--- app/src/main/java/com/baixd/app/framework/widget/ImageSwitchAndGalleryActivity.java
+++ app/src/main/java/com/baixd/app/framework/widget/ImageSwitchAndGalleryActivity.java
@@ -1,20 +1,115 @@
 package com.baixd.app.framework.widget;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
 import com.baixd.app.framework.R;
 
-public class ImageSwitchAndGalleryActivity extends ActionBarActivity {
+public class ImageSwitchAndGalleryActivity extends ActionBarActivity
+        implements ViewSwitcher.ViewFactory, AdapterView.OnItemSelectedListener {
+
+    private Integer[] mImageIds = {
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7};
+
+    private Integer[] mThumbIds = {
+            R.drawable.sample_thumb_0, R.drawable.sample_thumb_1,
+            R.drawable.sample_thumb_2, R.drawable.sample_thumb_3,
+            R.drawable.sample_thumb_4, R.drawable.sample_thumb_5,
+            R.drawable.sample_thumb_6, R.drawable.sample_thumb_7};
+
+    private ImageSwitcher mImageSwitcher;
+    private Gallery mGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_image_switch_and_gallery);
+
+        mImageSwitcher = (ImageSwitcher) findViewById(R.id.image_switcher);
+        mGallery = (Gallery) findViewById(R.id.gallery);
+
+        mImageSwitcher.setFactory(this);
+        mImageSwitcher.setInAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_in));
+        mImageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_out));
+
+        mGallery.setAdapter(new ImageAdapter(this));
+        mGallery.setOnItemSelectedListener(this);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+        mImageSwitcher.setImageResource(mImageIds[position]);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    @Override
+    public View makeView() {
+        ImageView iv = new ImageView(this);
+        iv.setBackgroundColor(0xFF000000);
+        iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        iv.setLayoutParams(new ImageSwitcher.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                ViewGroup.LayoutParams.FILL_PARENT));
+        return iv;
+    }
+
+
+    private class ImageAdapter extends BaseAdapter {
+
+        private Context mContext;
+
+        public ImageAdapter(Context ctx) {
+            mContext = ctx;
+        }
+
+        @Override
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+            ImageView iv = new ImageView(mContext);
+            iv.setImageResource(mThumbIds[position]);
+            iv.setAdjustViewBounds(true);
+            iv.setLayoutParams(new Gallery.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            iv.setBackgroundResource(R.drawable.picture_frame);
+
+            return iv;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,4 +132,7 @@ public class ImageSwitchAndGalleryActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
