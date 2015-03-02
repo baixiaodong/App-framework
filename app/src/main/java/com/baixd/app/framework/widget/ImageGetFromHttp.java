07b75a91ed16f652869da29f04ad59bfd97937d7
baixiaodong <baihsiang2010.d@gmail.com>
2015-03-02 14:20:01 +0800

    增加SD与网网络权限

---
+++ app/src/main/java/com/baixd/app/framework/widget/ImageGetFromHttp.java
@@ -0,0 +1,7 @@
+package com.baixd.app.framework.widget;
+
+/**
+ * Created by kk on 2015/3/2.
+ */
+public class ImageGetFromHttp {
+}
---
+++ app/src/main/java/com/baixd/app/framework/widget/ImageMemoryCache.java
@@ -0,0 +1,7 @@
+package com.baixd.app.framework.widget;
+
+/**
+ * Created by kk on 2015/2/28.
+ */
+public class ImageMemoryCache {
+}
